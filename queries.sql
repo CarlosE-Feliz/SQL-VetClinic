d6804fb44474e2d011928c3459ca1e1435c61423
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2022-01-10 15:53:26 -0400

    Add schema, data and queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,35 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT *
+from animals
+WHERE name like = '%mon'
+
+SELECT name 
+from animals 
+WHERE date_of_brith BETWEEN '01/01/2016' AND '01/01/2019'
+
+SELECT name
+from animals
+WHERE neutered = true
+AND escape_attempts < 3
+
+SELECT date_of_brith
+from name = 'Agumon'
+or name = 'Pikachu'
+
+SELECT name, escape_attempts
+from animals
+WHERE weight_kg > 10.5
+
+SELECT * from animals
+WHERE neutered = true
+
+SELECT * 
+from animals
+WHERE name != 'Gabumon'
+
+SELECT * 
+from animals
+WHERE weight_kg >= 10.4 
+AND weight_kg <= 17.3
+
